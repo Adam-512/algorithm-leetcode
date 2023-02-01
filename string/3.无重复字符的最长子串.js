416e1a6051df12d80a1dc86e6994df1085667fdf
adam <ch512006634@gmail.com>
2023-02-01 11:49:40 +0800

    滑动窗口模版-最大窗口、不重复子串

--- string/3.无重复字符的最长子串.js
+++ string/3.无重复字符的最长子串.js
@@ -1,27 +1,26 @@
-/*
- * @lc app=leetcode.cn id=3 lang=javascript
- *
- * [3] 无重复字符的最长子串
- */
-
-// @lc code=start
 /**
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
+var lengthOfLongestSubstring = function(s) {
+    //不重复字串，最大窗口
     let left = 0;
-    let mp = new Map();
-    let len = 0;
-    for (let right = 0; right < s.length; right++) {
-        let str = s[right]
-        if (mp.has(s[right])) {
-            left = Math.max(left, mp.get(str) + 1)
+    // let r = 0
+    let w = {}
+    let len = 0
+    for(let r=0;r<s.length;r++){
+        let str = s[r]
+        w[str] = w[str] ? ++w[str] : 1;
+        // console.log(w)
+        while(w[str] > 1){
+            let d = s[left]
+            left++
+            //
+            w[d]--
         }
-        len = Math.max(len, right - left + 1)
-        mp.set(str, right)
+        // console.log(s.slice(left,r))
+        //
+        len = Math.max(len,r - left + 1)
     }
     return len;
 };
-// @lc code=end
-
