1c208b44886c6e63bb3241b0a0ff6eb7f0e9bbba
adam <ch512006634@gmail.com>
2023-02-08 18:51:03 +0800

    三数和

--- array/15.三数之和.js
+++ array/15.三数之和.js
@@ -9,8 +9,31 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-
+var threeSum = function (nums) {
+    let res = []
+    let _nums = nums.sort((a, b) => a - b);
+    for (let i = 0; i < _nums.length; i++) {
+        let l = i + 1, r = _nums.length - 1;
+        if (_nums[i] > 0) break;
+        if (i > 0 && _nums[i] == _nums[i - 1]) continue
+        while (l < r) {
+            let sum = _nums[i] + _nums[l] + _nums[r]
+            if (sum < 0) {
+                l++
+            }
+            else if (sum > 0) {
+                r--
+            }
+            else {
+                res.push([_nums[i], _nums[l], _nums[r]])
+                while (l < r && _nums[l] == _nums[l + 1]) { l++ }
+                while (l < r && _nums[r] == _nums[r - 1]) { r-- }
+                l++
+                r--
+            }
+        }
+    }
+    return res
 };
 // @lc code=end
 
